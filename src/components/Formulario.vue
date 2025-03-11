312410315e329b381a54816751dd71ea7009bfd0
Flavio Irala <flavioiralap@gmail.com>
2025-03-11 13:45:36 -0400

    Continuação da Aula

---
+++ src/components/Formulario.vue
@@ -0,0 +1,24 @@
+<script setup>
+    const props = defineProps(['cadastrarTarefa', 'tarefaTemp', 'trocarFiltro', 'editaTarefaTempo'])
+</script>
+
+<template>
+    <form @submit.prevent="props.cadastraTarefa">
+        <div class="row">
+            <div class="col">
+                <input :value="props.tarefaTemp" @change="props.editaTarefaTempo" required
+                    type="text" placeholder="Digite a descrião da tarefa" class="form-control">
+            </div>
+            <div class="col-md-2">
+                <button class="btn btn-primary" type="submit">cadastrar</button>
+            </div>
+            <div class="col-md-2">
+                <select @change="props.trocarFiltro" class="form-control">
+                    <option value="todas">Todas tarefas</option>
+                    <option value="pendentes">Pendentes</option>
+                    <option value="finalizadas">Finalizadas</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template>
